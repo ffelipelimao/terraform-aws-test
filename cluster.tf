c2f0a53aaa81e396e1da3a2ff43a6d56bda936a3
Luiz Felipe Limao <lflimao@hotmail.com>
2023-01-25 18:18:16 -0300

    add eks cluster

--- cluster.tf
+++ cluster.tf
@@ -13,3 +13,55 @@ resource "aws_security_group" "sg"{
       Name = "${var.prefix}-sg"
     }
 }
+
+resource "aws_iam_role" "cluster" {
+    name = "${prefix}-${cluster_name}"
+    assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}  
+    POLICY
+}
+
+
+resource "aws_iam_role_policy_attachment" "cluster-AmazonEKSVPCResourceController" {
+  role = aws_iam_role.cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+}
+
+resource "aws_iam_role_policy_attachment" "cluster-AmazonEKSClusterPolicy" {
+  role = aws_iam_role.cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_cloudwatch_log_group" "log" {
+  name = "/aws/eks-terraform-course/${var.prefix}-${var.cluster_name}/cluster"
+  retention_in_days = var.retention_days
+}
+
+
+resource "aws_eks_cluster" "cluster" {
+  name = "${var.prefix}-${var.cluster_name}"
+  role_arn = aws_iam_role.cluster.arn
+  enabled_cluster_log_types = ["api","audit"]
+  vpc_config {
+      subnet_ids = var.subnet_ids 
+      security_group_ids = [aws_security_group.sg.id]
+  }
+  depends_on = [
+    aws_cloudwatch_log_group.log,
+    aws_iam_role_policy_attachment.cluster-AmazonEKSVPCResourceController,
+    aws_iam_role_policy_attachment.cluster-AmazonEKSClusterPolicy,
+  ]
+}
+
+
