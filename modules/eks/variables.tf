682860197af51f883c692479cec41ad3d1c42961
Luiz Felipe Limao <lflimao@hotmail.com>
2023-01-31 22:27:37 -0300

    Refactor to module scripts

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -1,6 +1,8 @@
+variable "vpc_id" {}
 variable "prefix" {}
 variable "cluster_name" {}
-variable retention_days {}
+variable "retention_days" {}
+variable "subnets_id" {}
 variable "desired_size" {}
 variable "max_size" {}
 variable "min_size" {}
